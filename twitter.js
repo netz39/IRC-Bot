db29e95e1281204796d905e616203dadd7092c70
Sebastian Mai <bastinat0r@bastinat0r.de>
2012-09-04 15:14:58 +0200

    colors

--- twitter.js
+++ twitter.js
@@ -38,7 +38,7 @@ var req = https.request(options, function(res) {
 			if(/^\{/.test(dat)) {
 				var tweet = JSON.parse(dat);
 				if(tweet.user) {
-					bot.say("#netz39",tweet.user.screen_name + ': ' + tweet.text);
+					bot.say("#netz39",irc.colors.warp('dark_green',tweet.user.screen_name + ': ' + tweet.text,'reset');
 				}
 			}
 		} catch (err) {
